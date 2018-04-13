c6561814082d5f57d134f145c3bd495fa87f3774
tushar <tusharwalzade216@users.noreply.github.com>
2018-04-13 20:01:27 +0530

    Added Email functionality
    Added an automated email functionality after uploading a file

--- gencsv.ps1
+++ gencsv.ps1
@@ -1,7 +1,26 @@
 $basePath = "C:\SQL_CSV_FTP_Uploader\"
+$uploadPath = "/Uploads/*"
 $outFile = "C:\Windows\Temp\sql_$(Get-Date -Format "yyyy-MM-dd").csv"
 $query = Get-Content -Path $($basePath + "query.sql")
-Invoke-Sqlcmd -ServerInstance 192.168.200.38 -Database ETL_Mappers -Username BI_USER -Password gsg@12345 -Query $query | Export-CSV $outFile -noType
+Invoke-Sqlcmd -ServerInstance XXX.XXX.XXX.XXX -Database YOURDB -Username BI_User -Password 'pass' -Query $query | Export-CSV $outFile -noType
+
+$Username = "abcd@yahoo.in";
+$Password = "";
+function Send-ToEmail([string]$file, [string]$srvpath) {
+
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = $Username;
+    $message.To.Add("abcd@sakon.com");
+    # $message.CC.Add("abcd@sakon.com");
+    $message.Subject = "Backup Uploaded - $($file)";
+    $message.Body = "Your Backup file - $($file) is successfully uploaded on -$($srvpath)";
+
+    $smtp = new-object Net.Mail.SmtpClient("smtp.mail.yahoo.com", "587");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message);
+    write-host "Mail Sent" ; 
+}
 
 # Load WinSCP .NET assembly
 Add-Type -Path $($basePath + "WinSCPnet.dll")
@@ -23,9 +42,10 @@ try
     $session.Open($sessionOptions)
 
     # Transfer files
-    $session.PutFiles($outFile, "/Uploads/*").Check()
+    $session.PutFiles($outFile, $uploadPath).Check()
 }
 finally
 {
+    Send-ToEmail -file $(Split-Path $outFile -leaf) -srvpath $($sessionOptions.HostName + $uploadPath)
     $session.Dispose()
 }
