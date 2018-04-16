486a239e68381ee221589eedf507a3efccd3d93d
tushar <tusharwalzade216@users.noreply.github.com>
2018-04-16 13:21:21 +0530

    Acknowledgement Updated
    Sends an acknowledgement to the specified emails after each successful/ erroneous upload

--- gencsv.ps1
+++ gencsv.ps1
@@ -1,19 +1,25 @@
 $basePath = "C:\SQL_CSV_FTP_Uploader\"
 $uploadPath = "/Uploads/*"
-$outFile = "C:\Windows\Temp\sql_$(Get-Date -Format "yyyy-MM-dd").csv"
+$outFile = "C:\Windows\Temp\data_$(Get-Date -Format "yyyy-MM-dd").csv"
 $query = Get-Content -Path $($basePath + "query.sql")
 Invoke-Sqlcmd -ServerInstance XXX.XXX.XXX.XXX -Database YOURDB -Username BI_User -Password 'pass' -Query $query | Export-CSV $outFile -noType
 
 $Username = "abcd@yahoo.in";
 $Password = "";
-function Send-ToEmail([string]$file, [string]$srvpath) {
+function Send-ToEmail([string]$file, [string]$srvpath, [string]$status) {
 
     $message = new-object Net.Mail.MailMessage;
     $message.From = $Username;
-    $message.To.Add("abcd@sakon.com");
-    # $message.CC.Add("abcd@sakon.com");
+    $message.To.Add("abcd@test.com");
+    # $message.CC.Add("xyz@test.com");
     $message.Subject = "Backup Uploaded - $($file)";
-    $message.Body = "Your Backup file - $($file) is successfully uploaded on -$($srvpath)";
+    $message.IsBodyHtml = $TRUE;
+    if ($status -eq $TRUE) {
+        $message.Body = "Your backup file - <b>$($file)</b> is successfully uploaded on location - <i>$($srvpath)</i>";
+    }
+    else {
+        $message.Body = "Something went wrong while uploading your backup file - <b>$($file)</b> on location - <i>$($srvpath)</i>...!";        
+    }
 
     $smtp = new-object Net.Mail.SmtpClient("smtp.mail.yahoo.com", "587");
     $smtp.EnableSSL = $true;
@@ -27,25 +33,26 @@ Add-Type -Path $($basePath + "WinSCPnet.dll")
 
 # Set up session options
 $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
-    Protocol = [WinSCP.Protocol]::Ftp
-    HostName = "localhost"
+    Protocol   = [WinSCP.Protocol]::Ftp
+    HostName   = "localhost"
     PortNumber = 21
-    UserName = "sakon"
-    Password = "sakon@123"
+    UserName   = "sakon"
+    Password   = "sakon@123"
 }
 
 $session = New-Object WinSCP.Session
 
-try
-{
+try {
     # Connect
     $session.Open($sessionOptions)
 
     # Transfer files
     $session.PutFiles($outFile, $uploadPath).Check()
+    Send-ToEmail -file $(Split-Path $outFile -leaf) -srvpath $($sessionOptions.HostName + $uploadPath) -status 'true'
 }
-finally
-{
-    Send-ToEmail -file $(Split-Path $outFile -leaf) -srvpath $($sessionOptions.HostName + $uploadPath)
+catch {
+    Send-ToEmail -file $(Split-Path $outFile -leaf) -srvpath $($sessionOptions.HostName + $uploadPath) -status 'false'
+}
+finally {
     $session.Dispose()
 }
