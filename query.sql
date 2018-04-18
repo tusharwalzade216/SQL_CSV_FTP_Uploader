acb3327c533c92d0c12fb55510a084aa7839c8f6
tushar <tusharwalzade216@users.noreply.github.com>
2018-04-18 18:46:59 +0530

    added multiple queries

--- query.sql
+++ query.sql
@@ -1,1 +1,5 @@
-SELECT * FROM [ETL_Mappers].[dbo].[Mapper_Log]
+SELECT * FROM [TestDB].[dbo].Invoice_Details where Clientname='ABC'
+SELECT * FROM [TestDB].[dbo].[Invoice_Summary] where Clientname='ABC'
+SELECT * FROM [TestDB].[dbo].OrderDetails where Clientname='ABC'
+SELECT * FROM [TestDB].[dbo].[ServiceContract] where Clientname='ABC'
+SELECT * FROM [TestDB].[dbo].ServiceDetailMaster where Clientname='ABC'
