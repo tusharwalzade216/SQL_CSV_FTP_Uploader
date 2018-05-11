c71eae6eebf02eb3fb759785528b7de611034488
tushar <tusharwalzade216@users.noreply.github.com>
2018-05-11 18:55:06 +0530

    updated path
    updated path - set to current folder

--- scheduleOnce.bat
+++ scheduleOnce.bat
@@ -1,2 +1,2 @@
 REM Schedule can be - MINUTE, HOURLY, DAILY, WEEKLY, ONCE, ONSTART, ONLOGON, ONIDLE, MONTHLY, ONEVENT
-schtasks /create /tn "SQL_CSV_FTP_Uploader" /tr C:\SQL_CSV_FTP_Uploader\execps.bat /sc minute /st 12:29:00 /ru System /rl highest
+schtasks /create /tn "SQL_CSV_FTP_Uploader" /tr .\execps.bat /sc minute /st 12:29:00 /ru System /rl highest
